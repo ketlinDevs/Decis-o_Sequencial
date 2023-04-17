3378160b09c17ff63ce3c66c466194bf9e2083cc
ketlinDevs <130712638+ketlinDevs@users.noreply.github.com>
2023-04-17 20:01:08 -0300

    Add files via upload

---
+++ DECISAO_SEQUENCIAL.py
@@ -0,0 +1,17 @@
+print('Decisão Sequencial')
+
+n = int(input("Digite um numero: "))
+
+if n == 1:
+    print("Você digitou o valor 1")
+
+elif n == 2:
+    print("Você digitou o valor 2")
+
+elif n < 1:
+    print("Você digitou um valor muito baixo")
+
+elif n > 2:
+    print("Você digitou um valor muito alto")
+
+print("Fim")
---
+++ Readme.md
@@ -0,0 +1,2 @@
+Livro: ALGORITMOS: LÓGICA PARA DESENVOLVIMENTO DE PROGRAMAÇÃO DE COMPUTADORES; 29° Edição; PG 77:
+Dos Autores José Augusto NG Manzano e Jayr Figueiredo De Oliveira.
